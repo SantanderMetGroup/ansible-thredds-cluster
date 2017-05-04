6938455ebd65e152e492a013fe6b4082be353bb3
Sonia Fernandez Tejeria <fernandez.sonia@outlook.es>
2017-05-04 15:34:34 +0100

    Edit catalog publishment, remove link to /opt/content/thredds, create new catalog in each TDS instance

--- roles/tds/files/deploy_tds_instance.sh
+++ roles/tds/files/deploy_tds_instance.sh
@@ -15,11 +15,12 @@ if [ ! -d "$INSTANCE_HOME" ]; then
   mkdir -p $INSTANCE_HOME/content/thredds
   mkdir -p $INSTANCE_HOME/content/thredds/cache
   mkdir -p $INSTANCE_HOME/content/thredds/logs
+  mkdir -p $INSTANCE_HOME/content/thredds/gws
   ln -s $CATALINA_HOME/content/thredds/public $INSTANCE_HOME/content/thredds/public
-  ln -s $CATALINA_HOME/content/thredds/catalog.xml $INSTANCE_HOME/content/thredds/catalog.xml
-  ln -s $CATALINA_HOME/content/thredds/enhancedCatalog.xml $INSTANCE_HOME/content/thredds/enhancedCatalog.xml
+#  ln -s $CATALINA_HOME/content/thredds/catalog.xml $INSTANCE_HOME/content/thredds/catalog.xml
+#  ln -s $CATALINA_HOME/content/thredds/enhancedCatalog.xml $INSTANCE_HOME/content/thredds/enhancedCatalog.xml
   ln -s $CATALINA_HOME/content/thredds/threddsConfig.xml $INSTANCE_HOME/content/thredds/threddsConfig.xml
   ln -s $CATALINA_HOME/content/thredds/wmsConfig.xml $INSTANCE_HOME/content/thredds/wmsConfig.xml
   ln -s $CATALINA_HOME/content/thredds/wmsConfig.dtd $INSTANCE_HOME/content/thredds/wmsConfig.dtd
-  ln -s $CATALINA_HOME/content/thredds/catalogs $INSTANCE_HOME/content/thredds/catalogs
+#  ln -s $CATALINA_HOME/content/thredds/catalogs $INSTANCE_HOME/content/thredds/catalogs
 fi
