ef8a241a7ce7a70e2f020bade855f0377eeb1d72
Sonia Fernandez Tejeria <fernandez.sonia@outlook.es>
2017-06-06 16:45:12 +0100

    Refactor TDS role: Install tomcat from RedHat repository

--- roles/tds/files/deploy_tds_instance.sh
+++ roles/tds/files/deploy_tds_instance.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
 CATALINA_HOME=$1
-INSTANCE_HOME=$2
+CATALINA_BASE=$2
 
-if [ ! -d "$INSTANCE_HOME" ]; then
-  mkdir -p $INSTANCE_HOME
-  mkdir -p $INSTANCE_HOME/temp
-  mkdir -p $INSTANCE_HOME/work
-  mkdir -p $INSTANCE_HOME/logs
-  ln -s $CATALINA_HOME/bin $INSTANCE_HOME/bin
-  cp -a $CATALINA_HOME/conf $INSTANCE_HOME/
-  ln -s $CATALINA_HOME/lib $INSTANCE_HOME/lib
-  ln -s $CATALINA_HOME/webapps $INSTANCE_HOME/webapps
-  mkdir -p $INSTANCE_HOME/content/thredds
-  mkdir -p $INSTANCE_HOME/content/thredds/cache
-  mkdir -p $INSTANCE_HOME/content/thredds/logs
-  mkdir -p $INSTANCE_HOME/content/thredds/gws
-  ln -s $CATALINA_HOME/content/thredds/public $INSTANCE_HOME/content/thredds/public
-#  ln -s $CATALINA_HOME/content/thredds/catalog.xml $INSTANCE_HOME/content/thredds/catalog.xml
-#  ln -s $CATALINA_HOME/content/thredds/enhancedCatalog.xml $INSTANCE_HOME/content/thredds/enhancedCatalog.xml
-  ln -s $CATALINA_HOME/content/thredds/threddsConfig.xml $INSTANCE_HOME/content/thredds/threddsConfig.xml
-  ln -s $CATALINA_HOME/content/thredds/wmsConfig.xml $INSTANCE_HOME/content/thredds/wmsConfig.xml
-  ln -s $CATALINA_HOME/content/thredds/wmsConfig.dtd $INSTANCE_HOME/content/thredds/wmsConfig.dtd
-#  ln -s $CATALINA_HOME/content/thredds/catalogs $INSTANCE_HOME/content/thredds/catalogs
+if [ ! -d "$CATALINA_BASE" ]; then
+  mkdir -p $CATALINA_BASE
+  mkdir -p $CATALINA_BASE/temp
+  mkdir -p $CATALINA_BASE/work
+  mkdir -p $CATALINA_BASE/logs
+  ln -s $CATALINA_HOME/bin $CATALINA_BASE/bin
+  cp -a $CATALINA_HOME/conf $CATALINA_BASE/
+  ln -s $CATALINA_HOME/lib $CATALINA_BASE/lib
+  ln -s $CATALINA_HOME/webapps $CATALINA_BASE/webapps
+  mkdir -p $CATALINA_BASE/content/thredds
+  mkdir -p $CATALINA_BASE/content/thredds/cache
+  mkdir -p $CATALINA_BASE/content/thredds/logs
+  mkdir -p $CATALINA_BASE/content/thredds/gws
+  ln -s $CATALINA_HOME/content/thredds/public $CATALINA_BASE/content/thredds/public
+  ln -s $CATALINA_HOME/content/thredds/threddsConfig.xml $CATALINA_BASE/content/thredds/threddsConfig.xml
+  ln -s $CATALINA_HOME/content/thredds/wmsConfig.xml $CATALINA_BASE/content/thredds/wmsConfig.xml
+  ln -s $CATALINA_HOME/content/thredds/wmsConfig.dtd $CATALINA_BASE/content/thredds/wmsConfig.dtd
 fi
