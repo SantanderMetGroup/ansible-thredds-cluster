339008afd4b245d8180f230fa2ab7538bbc6a8bb
zequi <zequi3000@hotmail.com>
2018-07-04 18:10:32 +0200

    Added request to no restricted dataset in jk-devel

--- scenarios/devel-jk/tests/test.sh
+++ scenarios/devel-jk/tests/test.sh
@@ -1,13 +1,14 @@
-DATASET='http://localhost:9000/thredds/dodsC/collection2/singleDataset.nc.html'
+DATASET1='http://localhost:9000/thredds/dodsC/collection1/singleDataset.nc.html'
+DATASET2='http://localhost:9000/thredds/dodsC/collection2/singleDataset.nc.html'
 RESTRICTED='http://localhost:9000/thredds/restrictedAccess/restringido'
 USER='alice'
 PASSWORD='1234'
 
-# CURL_OPTS='-s -o /dev/null -L --cookie-jar cookie.txt -b cookie.txt'
 CURL_OPTS='-v -o /dev/null -L --cookie-jar cookie.txt -b cookie.txt'
 
 if [ -f cookie.txt ]; then
 	rm cookie.txt
 fi
 
-curl $CURL_OPTS -u alice:1234 $DATASET
+curl $CURL_OPTS $DATASET1
+curl $CURL_OPTS -u alice:1234 $DATASET2
