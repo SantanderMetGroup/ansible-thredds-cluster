7b20c28f269fe9a36b5cd09f4d8b2787d73ceeda
zequi <zequi3000@hotmail.com>
2018-07-19 16:22:40 +0200

    Added role tds-cluster

--- scenarios/devel/tests/test.sh
+++ scenarios/devel/tests/test.sh
@@ -6,9 +6,5 @@ PASSWORD='1234'
 
 CURL_OPTS='-v -o /dev/null -L --cookie-jar cookie.txt -b cookie.txt'
 
-if [ -f cookie.txt ]; then
-	rm cookie.txt
-fi
-
 curl $CURL_OPTS $DATASET1
 curl $CURL_OPTS -u alice:1234 $DATASET2
