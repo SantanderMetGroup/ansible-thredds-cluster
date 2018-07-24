1a35e40fc13a4a4686a1f3c79a4052bc317e2250
Pablo-CC <p.celaya.crespo@gmail.com>
2018-07-24 11:28:12 +0200

    Include use of variables

--- scenarios/pablo/switching/files/lvs_script.sh
+++ scenarios/pablo/switching/files/lvs_script.sh
@@ -5,8 +5,8 @@ ipvsadm -C
 #Add service on load-balancer's private interface
 ipvsadm -A -t 192.0.2.17:8080 -s rr
 #Add real servers
+ipvsadm -a -t 192.0.2.17:8080 -r 192.0.2.18:8080 -g
 ipvsadm -a -t 192.0.2.17:8080 -r 192.0.2.15:8080 -g
-ipvsadm -a -t 192.0.2.17:8080 -r 192.0.2.16:8080 -g
 #Save rules
 ipvsadm -S -n > /etc/ipvsadm.rules
 #To restore configurarion after reboot type:
