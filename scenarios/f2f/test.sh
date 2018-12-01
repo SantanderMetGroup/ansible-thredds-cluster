452c0d9f6b551fd6683018f94509e5edf9970de4
zequi <zequi3000@hotmail.com>
2018-12-01 14:13:57 +0100

    Updated f2f test dataset

--- scenarios/f2f/test.sh
+++ scenarios/f2f/test.sh
@@ -4,7 +4,7 @@ set -e
 host=${1:-"esgf.test.es"}
 
 requests() {
-    dataset="http://$host/thredds/fileServer/collection2/drs/Test/v20181122/sftlf.nc"
+    dataset="https://$host/thredds/fileServer/esgcet/drs/Test/v20181201/sftlf.nc"
     openid_provider_submit="https://$host/esg-orp/j_spring_openid_security_check.htm"
     login_submit="https://$host/esgf-idp/idp/login_ids.htm"
 
@@ -18,13 +18,20 @@ requests() {
 
     > $cookie
     # Request dataset, this redirects us to the ORP
-    curl --fail $curl_opts $dataset
+    curl $curl_opts $dataset
+
+    # Submit the openid provider in the ORP, this redirects us to the IdP
+    curl $curl_opts \
+         -X POST \
+         -d "openid_identifier=https://esgf.test.es/esgf-idp/openid/" \
+         --referer "https://esgf.test.es/esg-orp/home.htm?redirect=https%3A%2F%2Fesgf.test.es%2Fthredds%2FfileServer%2Fesgcet%2Fdrs%2FTest%2Fv20181201%2Fsftlf.nc" \
+         $openid_provider_submit
 
     # Submit the openid provider in the ORP, this redirects us to the IdP
     curl $curl_opts \
 	 -X POST \
 	 -d "openid_identifier=https://esgf.test.es/esgf-idp/openid/" \
-	 --referer "https://esgf.test.es/esg-orp/home.htm?redirect=https%3A%2F%2Fesgf.test.es%2Fthredds%2FfileServer%2Fcollection2%2Fdrs%2FTest%2Fv20181122%2Fsftlf.nc" \
+	 --referer "https://esgf.test.es/esg-orp/home.htm?redirect=https%3A%2F%2Fesgf.test.es%2Fthredds%2FfileServer%2Fesgcet%2Fdrs%2FTest%2Fv20181201%2Fsftlf.nc" \
 	 $openid_provider_submit
 
     # Submit username and password in IdP
@@ -37,8 +44,4 @@ requests() {
 echo -n "HTTPS request for a netcdf file in $host"
 requests
 
-if [ $? -eq 0 ]; then
-        echo -e "\t[OK]"
-else
-        echo -e "\t[ERROR]"
-fi
+cat /tmp/cookie.txt
