20a05c9f44d9c568393d4f24e9884d449441932a
zequihg50 <zequi3000@hotmail.com>
2019-02-08 09:21:00 +0100

    First commit after refactor. modjk working

---
+++ scenarios/refactor/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+requests() {
+	DATASET1="http://$1:$2/thredds/dodsC/test/testData.nc.html"
+	DATASET2="http://$1:$2/thredds/dodsC/test/restrictedData.nc.html"
+	USER='alice'
+	PASSWORD='1234'
+
+	CURL_OPTS='--fail --referer ";auto" -v -o /dev/null -L --cookie-jar /tmp/cookie.txt -b /tmp/cookie.txt'
+
+	curl $CURL_OPTS $DATASET1
+	curl $CURL_OPTS -u alice:1234 $DATASET2
+}
+
+requests lb 8080
