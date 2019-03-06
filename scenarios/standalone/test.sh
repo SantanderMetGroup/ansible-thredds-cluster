610c8e89fa5a7ac6a1c295b87baab0c3bde1bbdd
zequihg50 <zequi3000@hotmail.com>
2019-03-06 10:19:09 +0100

    Updated scenario standalone. Created restart, stop and start in utils.

--- scenarios/standalone/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone --recursive -b devel https://github.com/SantanderMetGroup/ansible-thredds-cluster
-cd ansible-thredds-cluster/scenarios/zequi/standalone
-virtualenv virtualenv
-source virtualenv/bin/activate
-pip install --upgrade pip setuptools
-pip install ansible==2.5.*
-./run.sh -r ~/tds deploy
